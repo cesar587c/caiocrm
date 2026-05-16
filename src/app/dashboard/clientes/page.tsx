d77f9070adc4ab6cb9b2c520a6a10707d12d2c1b
Caio de Oliveira <caiomfdeoliveira@gmail.com>
2026-05-16 01:54:15 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@r

--- src/app/dashboard/clientes/page.tsx
+++ src/app/dashboard/clientes/page.tsx
@@ -1,9 +1,8 @@
-
 "use client";
 
 import { useState, useMemo, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@radix-ui/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import * as XLSX from 'xlsx';
 import {
